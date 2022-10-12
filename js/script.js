f08149df72b3b8bfd8109abe4c826ce4b91710f0
vlad <vladslobodanuk9@gmail.com>
2022-10-12 10:04:55 +0300

    first fille

--- js/script.js
+++ js/script.js
@@ -24,19 +24,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	//  Слайдер hero
 	$('.hero').slick({
-		autoplay: false,
-		arrows: false,
-		dots: true,
-		adaptiveHeight: true,
-		infinite: true,
-		mobileFirst: true,
-		swipeToSlide: true,
-		waitForAnimate: false,
-		rows: 1,
-		slidesPerRow: 1,
-		vertical: true,
-		verticalSwiping: true,
 
+
+		autoplay: true,
+		draggable: false,
+		swipe: false,
+		touchMove: false,
+		fade: true,
+		autoplaySpeed: 4000,
+		speed: 700,
+		arrows: false,
+		mobileFirst: true,
+		dots: true,
 	});
 	// Слайдер news
 	$('.news__cards').slick({
@@ -100,55 +99,56 @@ function initMap() {
 	});
 
 
-}
 
 
-// 
 
-const form = document.forms["form"];
-const formArr = Array.from(form);
-const validFormArr = [];
-const button = form.elements["button"];
+	// 
 
-formArr.forEach((el) => {
-	if (el.hasAttribute("data-reg")) {
-		el.setAttribute("is-valid", "0");
-		validFormArr.push(el);
-	}
-});
+	const form = document.forms["form"];
+	const formArr = Array.from(form);
+	const validFormArr = [];
+	const button = form.elements["button"];
 
-form.addEventListener("input", inputHandler);
-button.addEventListener("click", buttonHandler);
-
-function inputHandler({ target }) {
-	if (target.hasAttribute("data-reg")) {
-		inputCheck(target);
-	}
-}
-
-function inputCheck(el) {
-	const inputValue = el.value;
-	const inputReg = el.getAttribute("data-reg");
-	const reg = new RegExp(inputReg);
-	if (reg.test(inputValue)) {
-		el.setAttribute("is-valid", "1");
-		el.style.borderBottom = "2px solid rgb(0, 196, 0)";
-	} else {
-		el.setAttribute("is-valid", "0");
-		el.style.borderBottom = "2px solid rgb(255, 0, 0)";
-	}
-}
-
-function buttonHandler(e) {
-	const allValid = [];
-	validFormArr.forEach((el) => {
-		allValid.push(el.getAttribute("is-valid"));
-	});
-	const isAllValid = allValid.reduce((acc, current) => {
-		return acc && current;
+	formArr.forEach((el) => {
+		if (el.hasAttribute("data-reg")) {
+			el.setAttribute("is-valid", "0");
+			validFormArr.push(el);
+		}
 	});
 
-	if (!Boolean(Number(isAllValid))) {
-		e.preventDefault();
+	form.addEventListener("input", inputHandler);
+	button.addEventListener("click", buttonHandler);
+
+	function inputHandler({ target }) {
+		if (target.hasAttribute("data-reg")) {
+			inputCheck(target);
+		}
+	}
+
+	function inputCheck(el) {
+		const inputValue = el.value;
+		const inputReg = el.getAttribute("data-reg");
+		const reg = new RegExp(inputReg);
+		if (reg.test(inputValue)) {
+			el.setAttribute("is-valid", "1");
+			el.style.borderBottom = "2px solid rgb(0, 196, 0)";
+		} else {
+			el.setAttribute("is-valid", "0");
+			el.style.borderBottom = "2px solid rgb(255, 0, 0)";
+		}
+	}
+
+	function buttonHandler(e) {
+		const allValid = [];
+		validFormArr.forEach((el) => {
+			allValid.push(el.getAttribute("is-valid"));
+		});
+		const isAllValid = allValid.reduce((acc, current) => {
+			return acc && current;
+		});
+
+		if (!Boolean(Number(isAllValid))) {
+			e.preventDefault();
+		}
 	}
 }
